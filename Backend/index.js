ec0dddc29d0bc9cd8beac9862da68c4e8ac00f94
Shivanipatel80 <sp6260295729@gmail.com>
2026-08-11 22:45:17 +0530

    Fix production CORS

--- Backend/index.js
+++ Backend/index.js
@@ -12,18 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (
-        !origin ||
-        origin === "http://localhost:3000" ||
-        origin.endsWith(".vercel.app")
-      ) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
+    origin: true,
   })
 );
 
