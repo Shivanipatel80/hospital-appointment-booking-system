a626e97e9443f16833e1fb7115dfb30f04e0b5d9
Shivanipatel80 <sp6260295729@gmail.com>
2026-08-11 22:15:37 +0530

    Fix Render server port

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://medicare-backend-faq4.onrender.com",
+    baseURL: "https://medicare-backend-faq4.onrender.com/api",
 });
 
 API.interceptors.request.use((req)  => {
